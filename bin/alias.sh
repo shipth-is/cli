b70aebcd0ab028d4cc31dcbbe7615081f04ffd68
madebydavid <sutherland.dave@gmail.com>
2024-10-09 14:59:23 +0100

    Adding alias util

---
+++ bin/alias.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# you should run:
+#   source ./alias.sh
+
+scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+shipthis="$scriptDir/dev.js"
+
+alias shipthis="$shipthis"
