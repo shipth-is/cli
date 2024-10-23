306646e6855f4b1f3f98344341298c166f66b10c
madebydavid <sutherland.dave@gmail.com>
2024-10-23 20:34:55 +0100

    Seems to fix the text sometimes being shortened with each re-render

--- src/components/TruncatedText.tsx
+++ src/components/TruncatedText.tsx
@@ -1,5 +1,5 @@
 import {measureElement, Box, Text, TextProps} from 'ink'
-import {useRef} from 'react'
+import {useEffect, useRef, useState} from 'react'
 import stringLength from 'string-length'
 import stripAnsi from 'strip-ansi'
 
@@ -11,11 +11,18 @@ interface Props extends TextProps {
 export const TruncatedText = ({children, wrap, ...textPropsWithoutWrap}: Props) => {
   const ref = useRef()
 
+  const [width, setWidth] = useState<number | null>(null)
+
+  useEffect(() => {
+    if (!ref.current) return
+    const {width: measuredWidth} = measureElement(ref.current)
+    setWidth(measuredWidth)
+  }, [])
+
   // TODO: still not perfect as things get cut off earlier in the JobLogTail component?
   const getTruncated = (input: string) => {
     const withoutCrlf = input.replaceAll(/[\r\n]/g, '')
-    if (!ref.current) return withoutCrlf
-    const {width} = measureElement(ref.current)
+    if (width === null) return withoutCrlf
     const withoutAnsi = stripAnsi(withoutCrlf)
     const textLength = stringLength(withoutAnsi)
     return textLength > width ? withoutCrlf.substring(0, width) : withoutCrlf
