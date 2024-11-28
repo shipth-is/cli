8327cec5e8728cdb4409d80d565b8b34c5e021d7
David Sutherland <sutherland.dave@gmail.com>
2024-11-28 10:45:01 +0000

    Some hacking to the old readme gen

--- bin/readme.cjs
+++ bin/readme.cjs
@@ -1,6 +1,10 @@
 // Struggled to import and use the oclif classes in ES modules
 // Quick and dirty convert of https://github.com/oclif/oclif/blob/main/src/commands/readme.ts
+
+const loadHelpClass = require('@oclif/core').loadHelpClass
+
 const ReadmeGenerator = require('oclif/lib/readme-generator').default
+const HelpCompatibilityWrapper = require('oclif/lib/help-compatibility').HelpCompatibilityWrapper
 
 const path = require('path')
 const fs = require('fs-extra')
@@ -21,9 +25,17 @@ const FLAGS = {
   version: undefined,
 }
 
+const columns = Number.parseInt(process.env.COLUMNS || '120', 10)
+
 function compact(a) {
   // eslint-disable-next-line unicorn/prefer-native-coercion-functions
   return a.filter((a) => Boolean(a))
+}
+
+async function slugify(str) {
+  const {default: GithubSlugger} = await import('github-slugger')
+  const slugger = new GithubSlugger()
+  return slugger.slug(str)
 }
 
 class CustomGenerator extends ReadmeGenerator {
@@ -83,6 +95,102 @@ class CustomGenerator extends ReadmeGenerator {
         .join('\n')
         .trim() + '\n'
     )
+  }
+
+  async createTopicFile(file, topic, commands) {
+    //console.log('in the custom createTopicFile')
+    const oldBin = `\`${this.config.bin} ${topic.name}\``
+    const bin = `\`${this.config.bin} ${topic.name.replaceAll(':', ' ')}\``
+
+    const oldDoc =
+      [
+        bin,
+        '='.repeat(bin.length),
+        '',
+        render(topic.description || '', {config: this.config}).trim(),
+        '',
+        await this.commands(commands),
+      ]
+        .join('\n')
+        .trim() + '\n'
+
+    const doc =
+      [
+        render(topic.description || '', {config: this.config}).trim(),
+        '',
+        '### Usage',
+        '',
+        bin,
+        '',
+        '### Help output',
+        '',
+        await this.commands(commands),
+      ]
+        .join('\n')
+        .trim() + '\n'
+
+    await this.write(path.resolve(this.options.pluginDir ?? process.cwd(), file), doc)
+  }
+
+  renderCommand(c, HelpClass) {
+    //console.log('in our custom renderCommand')
+    const title = render(c.summary ?? c.description ?? '', {command: c, config: this.config})
+      .trim()
+      .split('\n')[0]
+    const help = new HelpClass(this.config, {maxWidth: columns, respectNoCacheDefault: true, stripAnsi: true})
+    const wrapper = new HelpCompatibilityWrapper(help)
+
+    const header = () => {
+      const usage = this.commandUsage(c)
+      return usage ? `## \`${this.config.bin} ${usage}\`` : `## \`${this.config.bin}\``
+    }
+
+    try {
+      // copy c to keep the command ID with colons, see:
+      // https://github.com/oclif/oclif/pull/1165#discussion_r1282305242
+      const command = {...c}
+
+      const res = compact([
+        //header(),
+        //title,
+        '```\n' + wrapper.formatCommand(c).trim() + '\n```',
+        //this.commandCode(command),
+      ]).join('\n\n')
+
+      //console.log(JSON.stringify(res, null, 2))
+
+      return res
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  async commands(commands) {
+    //console.trace()
+    //console.log('in our custom commands')
+    const helpClass = await loadHelpClass(this.config)
+    const res = [
+      // ...(await Promise.all(
+      //   commands.map(async (c) => {
+      //     const usage = this.commandUsage(c)
+      //     console.log(
+      //       'USAGE',
+      //       usage,
+      //       `* [\`${this.config.bin} ${usage}\`](#${await slugify(`${this.config.bin}-${usage}`)})`,
+      //     )
+      //     return usage
+      //       ? `* [\`${this.config.bin} ${usage}\`](#${await slugify(`${this.config.bin}-${usage}`)})`
+      //       : `* [\`${this.config.bin}\`](#${await slugify(`${this.config.bin}`)})`
+      //   }),
+      // )),
+      '',
+      ...commands.map((c) => this.renderCommand({...c}, helpClass)).map((s) => s.trim() + '\n'),
+    ]
+      .join('\n')
+      .trim()
+
+    //console.log(JSON.stringify(res, null, 2))
+    return res
   }
 }
 
