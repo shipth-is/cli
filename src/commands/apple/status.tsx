291d2e02289db1dde8152f4051e997c8be0cd77c
David Sutherland <sutherland.dave@gmail.com>
2024-11-28 10:50:01 +0000

    Allow running apple status when not authenticated on Apple developer portal

--- src/commands/apple/status.tsx
+++ src/commands/apple/status.tsx
@@ -1,9 +1,9 @@
-import {BaseAppleCommand} from '@cli/baseCommands/index.js'
+import {BaseAuthenticatedCommand} from '@cli/baseCommands/index.js'
 import {App, NextSteps, StatusTable} from '@cli/components/index.js'
 import {render} from 'ink'
 
 // TODO: allow to run if not authed so we can see if we are not authed?
-export default class AppleStatus extends BaseAppleCommand<typeof AppleStatus> {
+export default class AppleStatus extends BaseAuthenticatedCommand<typeof AppleStatus> {
   static override args = {}
 
   static override description = 'Shows the status of the Apple authentication and integration'
@@ -13,16 +13,33 @@ export default class AppleStatus extends BaseAppleCommand<typeof AppleStatus> {
   static override flags = {}
 
   public async run(): Promise<void> {
-    const authState = await this.refreshAppleAuthState()
-    const {session} = authState
-
-    const statuses = {
-      'Full Name': session.user.fullName,
-      'Provider Name': session.provider.name,
+    const checkAuth = async () => {
+      try {
+        const authState = await this.refreshAppleAuthState()
+        const {session} = authState
+        return {
+          isAuthenticatedOnApple: true,
+          session,
+        }
+      } catch {
+        return {
+          isAuthenticatedOnApple: false,
+          session: null,
+        }
+      }
     }
 
-    // TODO: steps
-    const steps: string[] = []
+    const {isAuthenticatedOnApple, session} = await checkAuth()
+
+    const statuses = {
+      'Authenticated on Apple Developer Portal': isAuthenticatedOnApple,
+      'Apple Full Name': session?.user?.fullName || 'Please authenticate',
+      'Apple Provider Name': session?.provider?.name || 'Please authenticate',
+    }
+
+    let steps = []
+
+    if (!isAuthenticatedOnApple) steps.push('$ shipthis apple login')
 
     // TODO: this looks ugly as shit
     render(
