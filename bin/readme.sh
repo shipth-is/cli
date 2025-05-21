11a69078e6f63034840ee1c564e2b268b084da36
Jamie Learmonth <jamie@boxlightmedia.com>
2025-05-21 12:55:43 +0100

    fix: use custom readme generator rather than oclif for github workflow

--- bin/readme.sh
+++ bin/readme.sh
@@ -5,11 +5,24 @@ scriptDir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd $scriptDir/..
 
 # We want to avoid overwriting the docs we have manually written
+# To force override run `OVERWRITE=true ./bin/readme.sh`
+OVERWRITE=${OVERWRITE:-false}
 
-bin/dev.js internal readme docs/ -d 2 --only docs/game --notDryRun --overWrite
-bin/dev.js internal readme docs/ -d 2 --only docs/apple/apiKey --notDryRun --overWrite
-bin/dev.js internal readme docs/ -d 2 --only docs/apple/certificate --notDryRun --overWrite
-bin/dev.js internal readme docs/ -d 2 --only docs/login.md --notDryRun --overWrite
-bin/dev.js internal readme docs/ -d 2 --only docs/status.md --notDryRun --overWrite
-bin/dev.js internal readme docs/ -d 2 --only docs/dashboard.md --notDryRun --overWrite
-bin/dev.js internal readme docs/ -d 2 --only docs/help.md --notDryRun --overWrite
+readme_command() {
+  local file=$1
+  local overwrite_option=""
+
+  if [ "$OVERWRITE" = true ]; then
+    overwrite_option="--overWrite"
+  fi
+
+  bin/dev.js internal readme docs/ -d 2 --only $file --notDryRun $overwrite_option
+}
+
+readme_command "docs/game"
+readme_command "docs/apple/apiKey"
+readme_command "docs/apple/certificate"
+readme_command "docs/login.md"
+readme_command "docs/status.md"
+readme_command "docs/dashboard.md"
+readme_command "docs/help.md"
