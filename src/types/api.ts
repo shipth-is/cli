e886b2ca37af05e1d1a3935621471a8a14e0cd98
David Sutherland <sutherland.dave@gmail.com>
2025-09-16 10:25:41 +0100

    Trigger a go build

--- src/types/api.ts
+++ src/types/api.ts
@@ -28,6 +28,7 @@ export type SelfWithJWT = {
 export enum Platform {
   ANDROID = 'ANDROID',
   IOS = 'IOS',
+  GO = 'GO',
 }
 
 export enum GameEngine {
@@ -177,6 +178,7 @@ export enum BuildType {
   AAB = 'AAB',
   APK = 'APK',
   IPA = 'IPA',
+  GO = 'GO',
 }
 
 export interface Build {
