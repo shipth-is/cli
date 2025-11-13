a0f17bfa68a11048b1567d108cbcba2ad48b15d7
David Sutherland <sutherland.dave@gmail.com>
2025-11-13 15:48:33 +0000

    Better formatting of runtime logs. Typing logs. Fixing ShipResult bug

--- src/utils/hooks/useGoRuntimeLogListener.ts
+++ src/utils/hooks/useGoRuntimeLogListener.ts
@@ -1,17 +1,44 @@
-import {WebSocketListener, useWebSocket} from './useWebSocket.js'
+import {useState} from 'react'
+import {DateTime} from 'luxon'
 
-export interface GoRuntimeLogListenerProps {
+import {WebSocketListener, useWebSocket} from './useWebSocket.js'
+import {RuntimeLogEntry} from '@cli/types/api.js'
+
+interface Props {
   projectId: string
   buildId: string
+  tailLength?: number
 }
 
-export function useGoRuntimeLogListener({projectId, buildId}: GoRuntimeLogListenerProps) {
+interface Response {
+  messages: RuntimeLogEntry[]
+  tail: RuntimeLogEntry[]
+}
+
+// Listens for Go runtime logs for a build via WebSocket and gives a tail
+export function useGoRuntimeLogListener({projectId, buildId, tailLength = 10}: Props): Response {
+  const [messages, setMessages] = useState<RuntimeLogEntry[]>([])
+  const [tail, setTail] = useState<RuntimeLogEntry[]>([])
+
   const listener: WebSocketListener = {
-    async eventHandler(pattern: string, rawLog: any) {
-      console.log(`[Go Runtime Log] ${JSON.stringify(rawLog)}`)
+    async eventHandler(_: string, rawLog: any) {
+      const log: RuntimeLogEntry = {
+        ...rawLog,
+        sentAt: DateTime.fromISO(rawLog.sentAt),
+      }
+      setMessages((prev) => [...prev, log])
+      setTail((prev) => {
+        const next = [...prev, log]
+        if (next.length > tailLength) next.shift()
+        return next
+      })
+      //console.log(log.message)
     },
+
     getPattern: () => [`project.${projectId}:build.${buildId}:runtime-log`],
   }
 
   useWebSocket([listener])
+
+  return {messages, tail}
 }
