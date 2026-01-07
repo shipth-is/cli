708ec49353f105528577c28b0eec2c94e4e9330f
David Sutherland <sutherland.dave@gmail.com>
2026-01-07 14:45:12 +0000

    Small refactor

---
+++ src/components/android/CreateInitialBuild/EnableGradle.tsx
@@ -0,0 +1,38 @@
+import {Box} from 'ink'
+import {getMajorVersion} from 'godot-export-presets'
+
+import {Markdown} from '@cli/components/index.js'
+import {getGodotVersion, getGradleBuildOptionKey} from '@cli/utils/godot.js'
+
+import {WEB_URL} from '@cli/constants/index.js'
+import {useSafeInput} from '@cli/utils/index.js'
+
+interface Props extends React.ComponentPropsWithoutRef<typeof Box> {
+  onConfirm?: () => void
+  onCancel?: () => void
+}
+
+export const EnableGradle = ({onConfirm, onCancel, ...boxProps}: Props) => {
+  const godotVersion = getGodotVersion()
+  const majorVersion = getMajorVersion(godotVersion)
+
+  useSafeInput(async (input) => {
+    if (input == 'y') return onConfirm && onConfirm()
+    if (input == 'n') return onCancel && onCancel()
+  })
+
+  return (
+    <Box flexDirection="column" gap={1} {...boxProps}>
+      <Box flexDirection="row" gap={1}>
+        <Markdown
+          filename="confirm-change-android-build-method.md.ejs"
+          templateVars={{
+            docsURL: `${WEB_URL}docs/guides/android-build-methods`,
+            godotVersion,
+            optionKey: getGradleBuildOptionKey(majorVersion),
+          }}
+        />
+      </Box>
+    </Box>
+  )
+}
